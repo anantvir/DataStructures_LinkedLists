9b492a70345a962c01ee90707101984771ae95f4
Anantvir Singh <anantvir91@gmail.com>
2019-06-17 00:07:05 -0400

    Linked List Data Structure its operations

--- linked_list.py
+++ linked_list.py
@@ -10,35 +10,75 @@ class LinkedList:
         self.current_ptr = None         # maintain a current pointer
         self.size = 0
     
-    def add_element_at_rear(self,element):
+    def add_item_at_rear(self,item):
         if self.head == None:           # list initially empty ?
-            newNode = Node(element)
+            newNode = Node(item)
             self.head = newNode         
             self.tail = newNode
             self.size += 1
         else:
-            newNode = Node(element)
+            newNode = Node(item)
             self.tail.link = newNode
             self.tail = newNode
             self.size += 1
         return newNode
     
+    def add_item_at_front(self,item):
+        if self.head == None:
+            newNode = Node(item)
+            self.head = newNode
+            self.tail = newNode
+            self.current_ptr = newNode
+            self.size += 1
+        else:
+            newNode = Node(item)
+            newNode.link = self.head
+            self.head = newNode
+            self.size += 1
+        return newNode
+    
+    def delete_item_from_front(self):
+        if self.head == None:
+            print('Cant delete from front. List is empty !')    # better raise exception here to avoid runtime errors
+        temp_pointer = self.head
+        self.head = self.head.link
+        temp_pointer.link = None
+        return temp_pointer.info
+
+
     def traverse_linked_list(self):
         temp_list =[]
         if self.current_ptr == None:
             self.current_ptr = self.head                #initialize current pointer to start/head of list
-        while self.current_ptr != None:                 # while last element with link = Null/None is not reached
-            temp_list.append(self.current_ptr.info)     #process the element
+        while self.current_ptr != None:                 # while last item with link = Null/None is not reached
+            temp_list.append(self.current_ptr.info)     #process the item
             self.current_ptr = self.current_ptr.link    #increment the pointer
         return temp_list
 
-
-    
+    def search_item(self,item):                         # Assume list is unsorted
+        location = None
+        if self.current_ptr == None:
+            self.current_ptr = self.head
+        while self.current_ptr != None:
+            if self.current_ptr.info == item:
+                location = self.current_ptr
+                self.current_ptr = self.current_ptr.link
+            else:
+                self.current_ptr = self.current_ptr.link
+        
+        if location == None:
+            print('Cannot find the item !')         # or raise any exception
+        return location
+           
 lst = LinkedList()
-lst.add_element_at_rear(2)
-lst.add_element_at_rear(5)
-lst.add_element_at_rear(8)
+lst.add_item_at_rear(2)
+lst.add_item_at_rear(5)
+lst.add_item_at_rear(8)
 
-temp = lst.traverse_linked_list()
-for x in range(len(temp)):
-    print(temp[x])
+print(lst.delete_item_from_front())
+print(lst.delete_item_from_front())
+print(lst.delete_item_from_front())
+print(lst.delete_item_from_front())
+# temp = lst.traverse_linked_list()
+# for x in range(len(temp)):
+#     print(temp[x])
